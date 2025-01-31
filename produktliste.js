813f0a33fb896a7ac521a0390b4d2438cdfbd7ff
Regitze Nykjaer <reny0001@stud.kea.dk>
2025-01-31 10:07:40 +0100

    list map

---
+++ produktliste.js
@@ -0,0 +1,27 @@
+let listContainer = document.querySelector("main");
+
+fetch(`https://kea-alt-del.dk/t7/api/products/`)
+  .then((response) => response.json())
+  .then((data) => showList(data));
+
+function showList(products) {
+  console.log(products);
+  let markup = "";
+  products
+    .map((product) => {
+      markup += `<div class="sub">
+              <div class="item">
+                <a href="produkt.html" target="_blank">
+                  <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="product image" />
+                </a>
+              </div>
+              <div class="product-text">
+                <h3>${product.productdisplayname}</h3>
+                <p>${product.price} DKK</p>
+              </div>
+            </div>`;
+    })
+    .join("");
+  console.log(markup);
+  listContainer.innerHTML = markup;
+}
