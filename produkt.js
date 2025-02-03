0823c4c17fd73829bedcdea9dae5d25680700200
Regitze Nykjaer <reny0001@stud.kea.dk>
2025-02-03 23:08:04 +0100

    add dynamisk index og singleproduct

--- produkt.js
+++ produkt.js
@@ -1,19 +1,19 @@
-let productId = 17005;
+const productId = new URLSearchParams(window.location.search).get("id");
+console.log("Product ID:", productId);
 let container = document.querySelector(".container");
 
 fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
   .then((response) => response.json())
   .then((data) => {
-    container.innerHTML = `
-       <div class="item">
-          <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="omen black heels" />
-        </div>
-        <div class="item_des">
-          <h1 class="product_name">${data.productdisplayname}</h1>
-          <h2>${data.price} DKK</h2>
-          <p>
-         ${data.description}
-          </p>
-        </div> 
-        `;
+    const markup = `
+      <div class="item">
+        <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="${data.productdisplayname}" />
+      </div>
+      <div class="item_des">
+        <h1 class="product_name">${data.productdisplayname}</h1>
+        <h2>${data.price} DKK</h2>
+        <p>${data.description}</p>
+      </div>
+    `;
+    container.innerHTML = markup;
   });
